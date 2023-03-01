cb601f9ad3c9dbb50802d9c2a754c3ff70e1e73e
Christopher Baum <christopherabaum@gmail.com>
2023-03-01 16:41:58 -0600

    feat: Add test data file

---
+++ test/test-data.js
@@ -0,0 +1,59 @@
+const testRooms = [
+    {
+    "number": 1,
+    "roomType": "residential suite",
+    "bidet": true,
+    "bedSize": "queen",
+    "numBeds": 1,
+    "costPerNight": 358.4
+    },
+    {
+    "number": 2,
+    "roomType": "suite",
+    "bidet": false,
+    "bedSize": "full",
+    "numBeds": 2,
+    "costPerNight": 477.38
+    },
+    {
+    "number": 3,
+    "roomType": "single room",
+    "bidet": false,
+    "bedSize": "king",
+    "numBeds": 1,
+    "costPerNight": 491.14
+    },
+    {
+    "number": 4,
+    "roomType": "single room",
+    "bidet": false,
+    "bedSize": "queen",
+    "numBeds": 1,
+    "costPerNight": 429.44
+    },
+    {
+    "number": 5,
+    "roomType": "single room",
+    "bidet": true,
+    "bedSize": "queen",
+    "numBeds": 2,
+    "costPerNight": 340.17
+    },
+];
+
+const testCustomers = [
+    {
+    "id": 1,
+    "name": "Leatha Ullrich"
+    },
+    {
+    "id": 2,
+    "name": "Rocio Schuster"
+    },
+    {
+    "id": 3,
+    "name": "Kelvin Schiller"
+    },
+];
+
+export default {testRooms, testCustomers}
