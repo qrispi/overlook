694ac28c31913f9efbc5d96f033c65cbb7b19de8
Christopher Baum <christopherabaum@gmail.com>
2023-03-02 17:35:52 -0600

    feat: Add function to utilize user methods in creating HTML elements

--- src/scripts.js
+++ src/scripts.js
@@ -9,6 +9,7 @@ let user;
 let hotel;
 const today = new Date();
 
+
 getData();
 
 function getData() {
@@ -17,7 +18,31 @@ function getData() {
         hotel = new Hotel(data[0].rooms, data[1].bookings);
         user = new Customer(data[2]);
     })
-    .then(logData);
+    .then(logData)
+    .then(displayUserReservations);
+}
+
+function displayUserReservations() {
+    const futureSection = document.getElementById('userFutureBookings');
+    const pastSection = document.getElementById('userPastBookings');
+    futureSection.innerHTML = '';
+    pastSection.innerHTML = '';
+    user.futureBookings.forEach(booking => {
+        futureSection.innerHTML += `
+        <article class="future-booking">
+            <img src="./images/hotel-room.png" alt="picture of booked room" class="booking-image">
+            <h5><i>ADD ROOM TYPE</i></h5>
+            <h5>Reserved on ${booking.date}</h5>
+        </article>`
+    });
+    user.pastBookings.forEach(booking => {
+        pastSection.innerHTML += `
+        <article class="past-booking">
+            <h5><i>ADD ROOM TYPE</i></h5>
+            <h5>Stayed in ${booking.date}</h5>
+        </article>`
+    });
+    document.getElementById('userAmountSpent').innerText = user.calculateMoneySpent(hotel.rooms);
 }
 
 // PURELY FOR TESTING
