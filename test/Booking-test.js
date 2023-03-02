a1a8fa05445bbc52995202662d97beac5ab2178d
Christopher Baum <christopherabaum@gmail.com>
2023-03-01 21:20:16 -0600

    feat: Complete constructor for Booking and set up test

--- test/Booking-test.js
+++ test/Booking-test.js
@@ -1,3 +1,18 @@
 import {expect} from 'chai';
-import testData from "../../test/test-data";
+// import { describe } from 'mocha';
+import testData from "./test-data";
 import Booking from '../src/classes/Booking';
+
+describe('Booking', () => {
+    let bookingData, booking1, booking2;
+
+    beforeEach(() => {
+        bookingData = testData.testBookings;
+        booking1 = new Booking(bookingData[0]);
+        booking2 = new Booking(bookingData[1]);
+    });
+
+    it('should be a function', () => {
+        expect(Booking).to.be.a('function');
+    });
+});
