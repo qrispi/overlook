2d3d50f6418c1beb4cdc713b978ee24b85ff9056
Christopher Baum <christopherabaum@gmail.com>
2023-03-06 14:13:25 -0600

    feat: Add method to Manager to update all customers

--- src/classes/Manager.js
+++ src/classes/Manager.js
@@ -4,6 +4,13 @@ class Manager {
     constructor(customersData) {
         this.customers = customersData.map(customer => new Customer(customer));
     }
+
+    updateCustomers(bookings, today) {
+        this.customers.forEach(customer => {
+            customer.sortMyBookings(bookings, today);
+            customer.calculateMoneySpent();
+        }); 
+    }
 }
 
 export default Manager;
