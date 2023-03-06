a335066036bb08f83cc8fb47f22606f5b9aa2e8f
Christopher Baum <christopherabaum@gmail.com>
2023-03-06 13:05:21 -0600

    feat: Connect Manager class and test file

--- test/Manager-test.js
+++ test/Manager-test.js
@@ -1,3 +1,19 @@
 import {expect} from 'chai';
 import Manager from '../src/classes/Manager';
 import testData from "./test-data";
+
+describe('Manager', () => {
+    let manager;
+
+    beforeEach(() => {
+        manager = new Manager();
+    });
+
+    it('should be a function', () => {
+        expect(Manager).to.be.a('function');
+    });
+
+    it('should be an instance of Manager', () => {
+        expect(manager).to.be.an.instanceOf(Manager);
+    });
+});
