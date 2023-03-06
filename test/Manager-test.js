9ab7237f21c944f5c9147c61bceb94dc98067f0b
Christopher Baum <christopherabaum@gmail.com>
2023-03-06 14:43:20 -0600

    feat: Add hook to Manager-test and test constructor

--- test/Manager-test.js
+++ test/Manager-test.js
@@ -1,12 +1,23 @@
 import {expect} from 'chai';
 import Manager from '../src/classes/Manager';
+import Customer from '../src/classes/Customer';
+import Hotel from '../src/classes/Hotel';
 import testData from "./test-data";
 
 describe('Manager', () => {
-    let manager;
+    let manager, hotel, customersData, customer1, customer2, bookingsData, roomsData;
+    const today = new Date();
+    today.setHours(0,0,0,0);
 
     beforeEach(() => {
-        manager = new Manager();
+        customersData = testData.testCustomers;
+        bookingsData = testData.testBookings;
+        roomsData = testData.testRooms;
+        hotel = new Hotel(roomsData);
+        hotel.updateBookings(bookingsData);
+        manager = new Manager(customersData);
+        customer1 = new Customer(customersData[0]);
+        customer2 = new Customer(customersData[1]);
     });
 
     it('should be a function', () => {
@@ -15,5 +26,12 @@ describe('Manager', () => {
 
     it('should be an instance of Manager', () => {
         expect(manager).to.be.an.instanceOf(Manager);
+    });
+
+    it('should store an array of Customers', () => {
+        expect(manager.customers).to.be.a('array');
+        expect(manager.customers.length).to.equal(3);
+        expect(manager.customers[0]).to.be.a.instanceOf(Customer);
+        expect(manager.customers[0]).to.deep.equal(customer1);
     });
 });
